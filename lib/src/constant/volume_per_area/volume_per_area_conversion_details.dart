72efaf139615f941ebe93657259349814456e3c4
Arif Rabbani <arif.93rabbani@gmail.com>
2019-06-11 14:27:12 +0530

    volume per area conversion details file format

--- lib/src/constant/volume_per_area/volume_per_area_conversion_details.dart
+++ lib/src/constant/volume_per_area/volume_per_area_conversion_details.dart
@@ -1,62 +1,46 @@
-import 'dart:math' show pow;
-
 import '../../enum/area_units.dart';
 import '../../enum/conversion_type.dart';
-import '../../enum/length_units.dart';
 import '../../enum/volume_per_area_units.dart';
 import '../../misc/global.dart';
 import '../../unit_converter.dart';
 
-final _perAcre = 1 /
-    getConversionDetail(
-      ConversionType.area,
-      AreaUnits.acre,
-    );
-final _perHectare = 1 /
-    getConversionDetail(
-      ConversionType.area,
-      AreaUnits.hectare,
-    );
-final _perSquareMile = 1 /
-    pow(
-      getConversionDetail(
-        ConversionType.length,
-        LengthUnits.mile,
-      ),
-      2,
-    );
-final _perSquareYard = 1 /
-    pow(
-      getConversionDetail(
-        ConversionType.length,
-        LengthUnits.yard,
-      ),
-      2,
-    );
-final _gallonToCubicMetre = getConversionDetail(
-  ConversionType.volume,
-  VolumeUnits.gallon_USLiquid,
-);
-final _litreToCubicMetre = getConversionDetail(
-  ConversionType.volume,
-  VolumeUnits.litre,
-);
+final _perAcreToPerSquareMetre =
+    1 / getConversionDetail(ConversionType.area, AreaUnits.acre);
+final _perHectareToPerSquareMetre =
+    1 / getConversionDetail(ConversionType.area, AreaUnits.hectare);
+final _perSquareMileToPerSquareMetre =
+    1 / getConversionDetail(ConversionType.area, AreaUnits.squareMile);
+final _perSquareYardToPerSquareMetre =
+    1 / getConversionDetail(ConversionType.area, AreaUnits.squareYard);
+
+final _gallonToCubicMetre =
+    getConversionDetail(ConversionType.volume, VolumeUnits.gallon_USLiquid);
+final _litreToCubicMetre =
+    getConversionDetail(ConversionType.volume, VolumeUnits.litre);
 
 final Map<VolumePerAreaUnits, double> volumePerAreaConversionDetails = {
   // Base unit
   VolumePerAreaUnits.cubicMetrePerSquareMetre: 1,
-  VolumePerAreaUnits.cubicMetrePerAcre: _perAcre,
-  VolumePerAreaUnits.cubicMetrePerHectare: _perHectare,
-  VolumePerAreaUnits.cubicMetrePerSquareMile: _perSquareMile,
-  VolumePerAreaUnits.cubicMetrePerSquareYard: _perSquareYard,
-  VolumePerAreaUnits.gallonPerAcre: _gallonToCubicMetre * _perAcre,
-  VolumePerAreaUnits.gallonPerHectare: _gallonToCubicMetre * _perHectare,
+  VolumePerAreaUnits.cubicMetrePerAcre: _perAcreToPerSquareMetre,
+  VolumePerAreaUnits.cubicMetrePerHectare: _perHectareToPerSquareMetre,
+  VolumePerAreaUnits.cubicMetrePerSquareMile: _perSquareMileToPerSquareMetre,
+  VolumePerAreaUnits.cubicMetrePerSquareYard: _perSquareYardToPerSquareMetre,
+  VolumePerAreaUnits.gallonPerAcre:
+      _gallonToCubicMetre * _perAcreToPerSquareMetre,
+  VolumePerAreaUnits.gallonPerHectare:
+      _gallonToCubicMetre * _perHectareToPerSquareMetre,
   VolumePerAreaUnits.gallonPerSquareMetre: _gallonToCubicMetre,
-  VolumePerAreaUnits.gallonPerSquareMile: _gallonToCubicMetre * _perSquareMile,
-  VolumePerAreaUnits.gallonPerSquareYard: _gallonToCubicMetre * _perSquareYard,
-  VolumePerAreaUnits.litrePerAcre: _litreToCubicMetre * _perAcre,
-  VolumePerAreaUnits.litrePerHectare: _litreToCubicMetre * _perHectare,
+  VolumePerAreaUnits.gallonPerSquareMile:
+      _gallonToCubicMetre * _perSquareMileToPerSquareMetre,
+  VolumePerAreaUnits.gallonPerSquareYard:
+      _gallonToCubicMetre * _perSquareYardToPerSquareMetre,
+  VolumePerAreaUnits.litrePerAcre:
+      _litreToCubicMetre * _perAcreToPerSquareMetre,
+  VolumePerAreaUnits.litrePerHectare:
+      _litreToCubicMetre * _perHectareToPerSquareMetre,
   VolumePerAreaUnits.litrePerSquareMetre: _litreToCubicMetre,
-  VolumePerAreaUnits.litrePerSquareMile: _litreToCubicMetre * _perSquareMile,
-  VolumePerAreaUnits.litrePerSquareYard: _litreToCubicMetre * _perSquareYard,
+  VolumePerAreaUnits.litrePerSquareMile:
+      _litreToCubicMetre * _perSquareMileToPerSquareMetre,
+  VolumePerAreaUnits.litrePerSquareYard:
+      _litreToCubicMetre * _perSquareYardToPerSquareMetre,
 };
