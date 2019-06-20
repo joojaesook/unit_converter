24625627ac8e33daf5e5942abad00fce62e6c0e5
Arif Rabbani <arif.93rabbani@gmail.com>
2019-06-20 23:11:10 +0530

    temperature converter in progress

--- lib/src/converter/formula_based_converter.dart
+++ lib/src/converter/formula_based_converter.dart
@@ -1,3 +1,5 @@
+import 'package:unit_converter/src/enum/temperature_unit.dart';
+
 import '../abstract/converter.dart';
 import '../enum/conversion_type.dart';
 import '../misc/global.dart';
@@ -12,10 +14,98 @@ class FormulaBasedConverter<T> extends Converter<T> {
     T from,
     T to,
   ) {
-    final double fromOffset = conversionFactor(type, from);
-    final double toOffset = conversionFactor(type, to);
-    value *= fromOffset;
-    value /= toOffset;
+    value = _convertToKelvin(from, value);
+    value = _convertFromKelvin(to, value);
     return value;
+  }
+
+  double _convertToKelvin(from, double value) {
+    switch (from as TemperatureUnit) {
+      case TemperatureUnit.kelvin:
+        return value;
+      case TemperatureUnit.degreeCelsius:
+        return _celsiusToKelvin(value);
+      case TemperatureUnit.degreeDelisle:
+        return _delisleToKelvin(value);
+      case TemperatureUnit.degreeFahrenheit:
+        return _fahrenheitToKelvin(value);
+      case TemperatureUnit.degreeNewton:
+        return _newtonToKelvin(value);
+      case TemperatureUnit.degreeRankine:
+        return _rankineToKelvin(value);
+    }
+  }
+
+  double _convertFromKelvin(to, double value) {
+    switch (to as TemperatureUnit) {
+      case TemperatureUnit.kelvin:
+        return value;
+      case TemperatureUnit.degreeCelsius:
+        return _celsiusFromKelvin(value);
+      case TemperatureUnit.degreeDelisle:
+        return _delisleFromKelvin(value);
+      case TemperatureUnit.degreeFahrenheit:
+        return _fahrenheitFromKelvin(value);
+      case TemperatureUnit.degreeNewton:
+        return _newtonFromKelvin(value);
+      case TemperatureUnit.degreeRankine:
+        return _rankineFromKelvin(value);
+    }
+  }
+
+  double _celsiusToKelvin(double value) {
+    return value + 273.15;
+  }
+
+  double _delisleToKelvin(double value) {
+    return 373.15 - (value * 2 / 3);
+  }
+
+  double _fahrenheitToKelvin(double value) {
+    return _celsiusToKelvin(_fahrenheitToCelsius(value));
+  }
+
+  double _newtonToKelvin(double value) {
+    return _celsiusToKelvin(_newtonToCelsius(value));
+  }
+
+  double _rankineToKelvin(double value) {
+    return value * 5 / 9;
+  }
+
+  double _fahrenheitToCelsius(double value) {
+    return ((value - 32) * 5) / 9;
+  }
+
+  double _newtonToCelsius(double value) {
+    return value / 0.33;
+  }
+
+  double _celsiusFromKelvin(double value) {
+    return value - 273.15;
+  }
+
+  double _delisleFromKelvin(double value) {
+    return (373.15 - value) * 3 / 2;
+  }
+
+  double _fahrenheitFromKelvin(double value) {
+    return _fahrenheitFromCelsius(_celsiusFromKelvin(value));
+  }
+
+  double _fahrenheitFromCelsius(double value) {
+    return (value * 9 / 5) + 32;
+  }
+
+  double _newtonFromKelvin(double value) {
+    return _newtonFromCelsius(_celsiusFromKelvin(value));
+  }
+
+  double _newtonFromCelsius(double value) {
+    return value * 0.33;
+  }
+
+  double _rankineFromKelvin(double value) {
+    return value * 9 / 5;
   }
 }
